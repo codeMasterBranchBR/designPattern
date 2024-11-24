a26c564203afdd6d6eff3de8bc5a4ed2c42624dc
Maxwell Chaves <maxwellbezerra112233@gmail.com>
2024-11-24 19:53:47 -0400

    Implemented new patterns Observer,State,Strategy,TemplateMethod,Visitor, change index description text.

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -8,8 +8,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Imagine Pattern was design to teach you pattern and apply in all languages.
       </>
     ),
   },
@@ -18,7 +17,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ImagineDesignPatterns lets you focus on your docs, and we&apos;ll do the chores. Go
         ahead and move your docs into the <code>docs</code> directory.
       </>
     ),
